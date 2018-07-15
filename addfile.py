bdacc23e3c6c475216af4375527d90d86857004b
DeepDrm <sergibic5@gmail.com>
2018-07-15 18:16:00 +0200

    P script

--- addfile.py
+++ addfile.py
@@ -1,8 +1,10 @@
 from subprocess import call
 
-fichero = input("Fichero a añadir: ")
+fichero = input("Fichero a sumar al: ")
 call(["git", "add","/home/ser/Desktop/TFG/TFG-GIT/SDN-UAH/"+fichero])
 
 mensaje = input("Mensaje del commit: ")
 call(["git", "commit","-m",mensaje])
 
+branch = input("Branch del commit: ")
+call(["git", "push","origin",branch])
