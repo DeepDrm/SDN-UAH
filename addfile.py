55102b7475bef2fe0d76929271f354062ec6fb55
Sergio <sergibic5@gmail.com>
2018-07-15 17:58:46 +0200

    Update addfile.py

--- addfile.py
+++ addfile.py
@@ -6,6 +6,3 @@ call(["git", "add","/home/ser/Desktop/TFG/TFG-GIT/SDN-UAH/"+fichero])
 mensaje = input("Mensaje del commit: ")
 call(["git", "commit","-m",mensaje])
 
-branch = input("Branch del commit: ")
-call(["git", "push","origin",branch])
-
